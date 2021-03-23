902fa442df982773dfd46656071b44f5da446d14
Isaac Johnston <isaac@superstruct.nz>
2021-03-23 18:44:17 +1300

    Add a test

--- test/re_highlight/core_test.cljs
+++ test/re_highlight/core_test.cljs
@@ -2,3 +2,6 @@
   (:require
     [clojure.test :refer [deftest is testing async use-fixtures]]
     [re-highlight.core :refer [highlight]]))
+
+(deftest returns-component
+  (is (instance? js/Function (highlight))))
