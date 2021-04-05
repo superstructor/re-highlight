ca031ccb8936b18da3acd2853d9f55dde9a0ef82
Isaac Johnston <isaac@superstruct.nz>
2021-04-06 11:53:33 +1200

    Upgrade karma to 6.3.2

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,6 +1,6 @@
 {:npm-deps     {"highlight.js"          "10.7.2"}
  :npm-dev-deps {"shadow-cljs"           "2.12.1"
-                "karma"                 "6.2.0"
+                "karma"                 "6.3.2"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
                 "karma-junit-reporter"  "2.0.1"}}
