ec46c76bb6cfd4a73db263ac03748c8f2634a853
Isaac Johnston <isaac@superstruct.nz>
2021-06-23 10:50:09 +1200

    Upgrade shadow-cljs to 2.14.5 and ClojureScript to 1.10.866

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license     {:name "MIT"}
 
   :dependencies [[org.clojure/clojure       "1.10.3"   :scope "provided"]
-                 [org.clojure/clojurescript "1.10.844" :scope "provided"
+                 [org.clojure/clojurescript "1.10.866" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library
                                org.clojure/google-closure-library-third-party]]
-                 [thheller/shadow-cljs      "2.12.4"  :scope "provided"]
+                 [thheller/shadow-cljs      "2.14.5"   :scope "provided"]
                  [reagent                   "1.0.0"    :scope "provided"]
                  [re-com                    "2.13.2"   :scope "provided"]]
 
--- src/deps.cljs
+++ src/deps.cljs
@@ -1,5 +1,5 @@
 {:npm-deps     {"highlight.js"          "10.7.2"}
- :npm-dev-deps {"shadow-cljs"           "2.12.4"
+ :npm-dev-deps {"shadow-cljs"           "2.14.5"
                 "karma"                 "6.3.2"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
