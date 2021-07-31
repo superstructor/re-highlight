5d40a3676593f756750bca55585b1b95f03c4b65
Isaac Johnston <isaac@superstruct.nz>
2021-07-31 13:10:25 +1200

    Upgrade ClojureScript to 1.10.879 and shadow-cljs to 2.15.2

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license     {:name "MIT"}
 
   :dependencies [[org.clojure/clojure       "1.10.3"   :scope "provided"]
-                 [org.clojure/clojurescript "1.10.866" :scope "provided"
+                 [org.clojure/clojurescript "1.10.879" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library
                                org.clojure/google-closure-library-third-party]]
-                 [thheller/shadow-cljs      "2.15.1"   :scope "provided"]
+                 [thheller/shadow-cljs      "2.15.2"   :scope "provided"]
                  [reagent                   "1.1.0"    :scope "provided"]
                  [re-com                    "2.13.2"   :scope "provided"]]
 
