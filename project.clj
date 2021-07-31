14de40cc8fe7ca7e835fa2786fc88a0010534081
Isaac Johnston <isaac@superstruct.nz>
2021-07-31 13:11:29 +1200

    Upgrade lein-git-inject to 0.0.15

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [reagent                   "1.1.0"    :scope "provided"]
                  [re-com                    "2.13.2"   :scope "provided"]]
 
-  :plugins [[day8/lein-git-inject "0.0.14"]
+  :plugins [[day8/lein-git-inject "0.0.15"]
             [lein-shadow          "0.4.0"]
             [lein-shell           "0.5.0"]]
 
